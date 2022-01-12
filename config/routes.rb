ae9cfe1b70159805000fd0a47e7922737a9388bf
EC2 Default User <ec2-user@ip-172-31-47-134.ec2.internal>
2022-01-12 20:12:04 +0000

    [add]第11章

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get '/top'=>"homes#top"
+  get 'top'=>"homes#top"
   post 'lists'=>'lists#create'
   get 'lists'=>'lists#index'
   get 'lists/:id'=>'lists#show',as:'list'
